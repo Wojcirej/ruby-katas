596837ac33fae09742e8167bc992e44ddb1fe197
Wojcirej <wojcirej@gmail.com>
2018-11-12 17:43:00 +0100

    RGB to hex conversion.

---
+++ rgb_to_hex_conversion/lib/rgb.rb
@@ -0,0 +1,12 @@
+def rgb(r, g, b)
+  r = set_in_range(r)
+  g = set_in_range(g)
+  b = set_in_range(b)
+  (r.to_s(16).rjust(2, '0') + g.to_s(16).rjust(2, '0') + b.to_s(16).rjust(2, '0')).upcase
+end
+
+def set_in_range(number)
+  return number if number.between?(0, 255)
+  return 255 if number > 255
+  return 0 if number < 0
+end
