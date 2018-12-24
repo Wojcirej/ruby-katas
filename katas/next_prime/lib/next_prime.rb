9820bff538ee92ee3c97472fb665a745ddcb0e3b
Wojcirej <wojcirej@gmail.com>
2018-12-24 12:23:26 +0100

    Next prime - refactoring.

--- katas/next_prime/lib/next_prime.rb
+++ katas/next_prime/lib/next_prime.rb
@@ -1,10 +1,4 @@
 require 'prime'
 def next_prime(n)
-  return 2 if n < 2
-  primes = Prime.each(n).map { |prime| prime }
-  next_potential_prime = primes.last + 1
-  while true
-    return next_potential_prime if Prime.prime?(next_potential_prime)
-    next_potential_prime += 1
-  end
+  Prime.find { |prime| prime > n }
 end
